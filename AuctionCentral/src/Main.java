ec897ff832bdfffd2dd57928d5a19fecc2a6641b
Keith Lueneburg <keith.lueneburg@gmail.com>
2014-02-21 19:01:51 -0800

    Initial commit

---
+++ AuctionCentral/src/Main.java
@@ -0,0 +1,16 @@
+
+/*
+ * Setting up GitHub repo..
+ */
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
