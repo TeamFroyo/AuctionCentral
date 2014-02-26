c0d330a202e377feeab2cc78d1ecf6f94382bd90
Keith Lueneburg <keith.lueneburg@gmail.com>
2014-02-25 19:52:42 -0800

    Did a test run of Documentation generation. Created test class for CreditCard.

--- AuctionCentral/src/bidding/CreditCard.java
+++ AuctionCentral/src/bidding/CreditCard.java
@@ -4,7 +4,12 @@ package bidding;
  * Class: CreditCard
  * 
  * Represent a credit card that a bidder will use to pay for a
- * bid that they win.
+ * item that they win the bidding for.
+ * 
+ * TCSS 360 - Software Development and Quality Assurance
+ * University of Washington, Tacoma
+ * Winter 2014
+ * Instructor: Dr. Adwoa Donyina
  * 
  * @author Keith Lueneburg
  *
@@ -40,13 +45,61 @@ public final class CreditCard {
 	 */
 	private String myBank;	
 		
+	/**
+	 * Create a new CreditCard object.
+	 * <p>
+	 * <dt><b> Precondition: We need a CreditCard object created. </b><dd>
+	 * <dt><b> Postcondition: The CreditCard has been initialized 
+	 * properly </b><dd>
+	 * 
+	 * @param aCardNum The card number.
+	 * @param anExpDate The expiration date.
+	 * @param aCSC The CSC code.
+	 * @param aCardHolder The card holder.
+	 * @param anAddress The card holder's address.
+	 * @param aBank The bank the card belongs to.
+	 * 
+	 * @return The CreditCard object.
+	 */
+	public CreditCard(long aCardNum, int anExpDate, int aCSC,
+			String aCardHolder, Address anAddress, String aBank){
+		myCardNum = aCardNum;
+		myExpDate = anExpDate;
+		myCSC = aCSC;
+		myCardHolder = aCardHolder;
+		myAddress = anAddress;
+		myBank = aBank;
+	}
+	
+	/**
+	 * Check to see if this CreditCard matches another CreditCard.
+	 * <p>
+	 * <dt><b> Precondition: CreditCard has been initialized. </b><dd>
+	 * <dt><b> Postcondition: The CreditCard has not been changed. </b><dd>
+	 * 
+	 * @param anObject The object to compare this CreditCard with.
+	 * 
+	 * @return True if the other object is also a CreditCard, and it represents
+	 * the same card as the card we are checking.
+	 */
+	@Override
 	public boolean equals(Object anObject) {
 		
 		return false;
 	}
 	
+	/**
+	 * Get a String representation of the CreditCard.
+	 * <p>
+	 * <dt><b> Precondition: CreditCard has been initialized. </b><dd>
+	 * <dt><b> Postcondition: The CreditCard has not been changed. </b><dd>
+	 * 
+	 * @return A string representation of the card.
+	 */
+	@Override
 	public String toString() {
-		
+		StringBuilder sb = new StringBuilder();
+		sb.append("");
 		return "";
 	}
 }
---
+++ AuctionCentral/src/test/CreditCardTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Class: CreditCardTest
+ * 
+ * Contains test for class: CreditCard
+ * 
+ * TCSS 360 - Software Development and Quality Assurance
+ * University of Washington, Tacoma
+ * Winter 2014
+ * Instructor: Dr. Adwoa Donyina
+ * 
+ * @author Keith Lueneburg
+ *
+ */
+public class CreditCardTest {
+
+	/**
+	 * Test method for {@link bidding.CreditCard#CreditCard(long, int, int, java.lang.String, bidding.Address, java.lang.String)}.
+	 */
+	@Test
+	public void testCreditCard() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bidding.CreditCard#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link bidding.CreditCard#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
